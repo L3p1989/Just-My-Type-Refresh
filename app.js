050f6f0ab9e91efbc60f4753deb7ae1deb0536d4
L3p1989 <phogan110@live.com>
2021-03-08 22:55:39 -0700

    created const keybrdUpper. hid keybrdUpper. created const keybrdLower

---
+++ app.js
@@ -0,0 +1,6 @@
+//grab keyboard upper container
+const keybrdUpper = $("#keyboard-upper-container");
+//hide keybrdUpper
+keybrdUpper.hide();
+//grab keyboard-lower-container
+const keybrdLower = $("#keyboard-lower-container");
