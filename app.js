b6fafd920f05438fe5ed68ab3eb9e70f6bc13930
L3p1989 <phogan110@live.com>
2021-03-08 23:19:45 -0700

    created const body. created keydown/keyup event listener

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ const keybrdUpper = $("#keyboard-upper-container");
 keybrdUpper.hide();
 //grab keyboard-lower-container
 const keybrdLower = $("#keyboard-lower-container");
+//grab body
+const body = $('body')
+//keydown function
+body.keydown((e) => {
+    //if shift is pressed shows keybrdUpper and hides keybrdLower
+    e.keyCode == 16 ? keybrdUpper.toggle() && keybrdLower.toggle() : null
+})
+$(this).keyup((e) => {
+    //if shift is let go hides keybrdUpper and shows keybrdLower
+    e.keyCode == 16 ? keybrdUpper.toggle() && keybrdLower.toggle() : null
+});
