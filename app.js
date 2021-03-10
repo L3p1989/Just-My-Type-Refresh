740199af97aed412fc3c2b4324ec94cca1b08fb8
L3p1989 <phogan110@live.com>
2021-03-09 18:00:15 -0700

    created a counter for sentences. added sentence array to #sentence based on sentenceCount

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-//Sentences
+//Sentences array
 const sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+//counter for sentences
+let sentenceCount = 0;
+//add sentences array to #sentence
+$("#sentence").append(sentences[sentenceCount]);
 //grab keyboard upper container
 const keybrdUpper = $("#keyboard-upper-container");
 //hide keybrdUpper
