eb1755b2b45d17a4d9ea4bc4e9d057c5ce8f50b8
L3p1989 <phogan110@live.com>
2021-03-09 17:50:23 -0700

    corrected comments. created a keypressed event listener for body. created an animation function that animates keys pressed

--- app.js
+++ app.js
@@ -6,12 +6,26 @@ keybrdUpper.hide();
 const keybrdLower = $("#keyboard-lower-container");
 //grab body
 const body = $('body')
-//keydown function
+//keydown event listener
 body.keydown((e) => {
     //if shift is pressed shows keybrdUpper and hides keybrdLower
-    e.keyCode == 16 ? keybrdUpper.toggle() && keybrdLower.toggle() : null
-})
+    e.keyCode == 16 ? keybrdUpper.show() && keybrdLower.hide() : null
+});
+//keyup event listener
 $(this).keyup((e) => {
     //if shift is let go hides keybrdUpper and shows keybrdLower
-    e.keyCode == 16 ? keybrdUpper.toggle() && keybrdLower.toggle() : null
+    e.keyCode == 16 ? keybrdUpper.hide() && keybrdLower.show() : null
 });
+//keypress event listener
+body.keypress((e) => {
+    $(`#${e.which}`).animate({
+        padding: "26px",
+        borderRadius: "8px"
+    }, 50,
+    function() {
+        $(this).animate({
+            padding: "24px",
+            borderRadius: "6px"
+        }, 50);
+    })
+})
