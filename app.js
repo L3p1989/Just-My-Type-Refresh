5d092260a729fc181688f85eedc1bd2061b508c4
L3p1989 <phogan110@live.com>
2021-03-09 17:53:46 -0700

    copied sentences array

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//Sentences
+const sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
 //grab keyboard upper container
 const keybrdUpper = $("#keyboard-upper-container");
 //hide keybrdUpper
